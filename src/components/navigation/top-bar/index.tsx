35bb97f7b03b3d4918b7a6fb0c6ed0b8f4ae41e1
Mishra Ajaya <mishraajaya@MishranoMacBook-Pro.local>
2022-07-18 23:02:35 +0900

    adding top navbar

--- src/components/navigation/top-bar/index.tsx
+++ src/components/navigation/top-bar/index.tsx
@@ -1,6 +1,21 @@
+import { ReactComponent as Logo } from 'assets/svgs/logo.svg'
+
+import './styles/_top-bar.scss'
+
 const TopNavBar = () => {
   return (
-    <div>Top Nav Bar</div>
+    <div className='top-navbar-container'>
+      <div className='left-container'>
+        <div className="logo-container">
+          <Logo />
+        </div>
+      </div>
+      <div className='right-container'>
+        <div className='right-menu-item'>Share</div>
+        <div className='right-menu-item'>Upload</div>
+        <div className='right-menu-item'>Settings</div>
+      </div>
+    </div>
   )
 }
 
